d54c42bde6472341fd9e76c599fb9ed646350cfc
dezhiliu <904139492@qq.com>
2016-08-14 01:48:14 +0800

    add tool method

--- src/main/java/com/ldz/util/DateUtil.java
+++ src/main/java/com/ldz/util/DateUtil.java
@@ -1,5 +1,8 @@
 package com.ldz.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -8,6 +11,8 @@ import java.util.Date;
  * @author Jenkin
  */
 public class DateUtil {
+
+	public static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss:SSSZ";
 
 	/**
 	 * turn millisecond timeStamp to Date Object
@@ -27,5 +32,54 @@ public class DateUtil {
 	 */
 	public static long Date2Timestamp(Date date) {
 		return date.getTime();
+	}
+
+	/**
+	 * turn Date Object to String use regular format
+	 * 
+	 * @param date
+	 * @param format
+	 * @return
+	 */
+	public static String Date2String(Date date) {
+		return Date2String(date, DEFAULT_FORMAT);
+	}
+
+	/**
+	 * turn Date Object to String use regular format
+	 * 
+	 * @param date
+	 * @param format
+	 * @return
+	 */
+	public static String Date2String(Date date, String format) {
+		DateFormat simformat = new SimpleDateFormat(format);
+		return simformat.format(date);
+	}
+
+	/**
+	 * turn String to Date Object use regular format
+	 * 
+	 * @param date
+	 * @param format
+	 * @return
+	 * @throws ParseException
+	 */
+	public static Date string2Date(String date) throws ParseException {
+		return string2Date(date, DEFAULT_FORMAT);
+	}
+
+	/**
+	 * turn String to Date Object use regular format
+	 * 
+	 * @param date
+	 * @param format
+	 * @return
+	 * @throws ParseException
+	 */
+	public static Date string2Date(String date, String format)
+			throws ParseException {
+		DateFormat simformat = new SimpleDateFormat(format);
+		return simformat.parse(date);
 	}
 }
